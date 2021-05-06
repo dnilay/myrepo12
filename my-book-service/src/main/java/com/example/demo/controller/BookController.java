f2a9f3d272a55d69cde22937a851ec9975989735
Nilay <t-nilay1@cognizant.com>
2021-05-06 17:02:02 +0530

    added jwt

--- my-book-service/src/main/java/com/example/demo/controller/BookController.java
+++ my-book-service/src/main/java/com/example/demo/controller/BookController.java
@@ -8,6 +8,11 @@ import javax.validation.constraints.Min;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +22,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +31,10 @@ import com.example.demo.error.BookNotFoundException;
 import com.example.demo.error.BookUnSupportedFieldPatchException;
 import com.example.demo.model.Book;
 import com.example.demo.repo.BookRepository;
+import com.example.demo.service.MyUserDetailsService;
+import com.example.demo.ui.AuthenticationRequest;
+import com.example.demo.ui.AuthenticationResponse;
+import com.example.demo.util.JwtUtil;
 
 @RestController
 @Validated
@@ -31,7 +42,35 @@ public class BookController {
 
     @Autowired
     private BookRepository repository;
+    @Autowired
+	private AuthenticationManager authenticationManager;
 
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+			);
+		}
+		catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
     // Find
     @GetMapping("/books")
     List<Book> findAll() {
